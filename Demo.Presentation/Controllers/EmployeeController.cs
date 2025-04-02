7a0435f2c9b6b6702a8b8fbc1bb4ffe46f31c1a6
joeshaker <youssefshaker296@gmail.com>
2025-04-02 07:10:17 +0200

    EditEmp&Details

--- Demo.Presentation/Controllers/EmployeeController.cs
+++ Demo.Presentation/Controllers/EmployeeController.cs
@@ -1,9 +1,12 @@
 ﻿using DataAccess.Models.EmployeeModel;
+using DataAccess.Models.Shared.Enums;
 using Demo.BusinessLogic.DataTransferObjects.DepartmentDataTransferDto;
 using Demo.BusinessLogic.DataTransferObjects.EmployeDataTransferDto;
 using Demo.BusinessLogic.Services.Classes;
 using Demo.BusinessLogic.Services.Interfaces;
+using Demo.Presentation.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Demo.Presentation.Controllers
 {
@@ -49,6 +52,71 @@ namespace Demo.Presentation.Controllers
 
             }
             return View(createdEmplopyeeDto);
+        }
+        #endregion
+        #region Details of Emp
+        public IActionResult Details(int ? id) {
+            if (!id.HasValue) return BadRequest();
+            var employees = _employeeServices.GetEmployeeById(id.Value);
+            return employees is null?NotFound() : View(employees);
+        }
+        #endregion
+        #region Edit
+        [HttpGet]
+        public IActionResult Edit(int ?id) {
+            if (!id.HasValue) return BadRequest();
+            var employee=_employeeServices.GetEmployeeById(id.Value);
+            if(employee is null) return NotFound();
+            var employeedto = new UpdateEmployeeDto()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                HiringDate= employee.HiringDate,
+                Email = employee.Email,
+                Gender= Enum.Parse<Gender>(employee.Gender),
+                EmployeeType=Enum.Parse<EmployeeType>(employee.EmployeeType),
+                PhoneNumber= employee.PhoneNumber,
+                Salary= employee.Salary,
+                Address= employee.Address,
+                Age=employee.Age,
+                IsActive=employee.IsActive
+            };
+            return View(employeedto);
+        }
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int? id, UpdateEmployeeDto updateEmployee) 
+        {
+            if (!id.HasValue|| id!=updateEmployee.Id) return BadRequest();
+            if(!ModelState.IsValid) return  View(updateEmployee);
+            try 
+            {
+                var result=_employeeServices.UpdateEmployee(updateEmployee);
+                if (result > 0) 
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Employee Can;t be Updated");
+                    return View(updateEmployee);
+
+
+                }
+            } 
+            catch (Exception ex) 
+            {
+                if (environment.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                    return View(updateEmployee);
+                }
+                else
+                {
+                    logger.LogError(ex.Message);
+                    return View("Error View", ex);
+                }
+
+            }
         }
         #endregion
     }
